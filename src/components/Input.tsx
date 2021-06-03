52018a90a561ef78029c330139df22f00f647c9f
Natan Fernandes <natanfernandes@protonmail.com>
2021-06-03 18:20:03 -0300

    add: componente input, fonte Inter, estilos globais

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from '../styles/components/Input.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
-export function Input() {
+interface ButtonProps {
+  id: string;
+  placeholder: string;
+  icon: IconDefinition;
+  size?: number;
+}
+
+export function Input(props: ButtonProps) {
+
+  const [inputValue, setInputValue] = useState('');
+
   return (
-    <>
-      <FontAwesomeIcon icon={faCoffee}/>
-    </>
+    <div className={`${styles.inputContainer} ${inputValue.length > 0 ? styles.filled : null }`}>
+      <FontAwesomeIcon icon={ props.icon }/>
+      <input id={props.id} placeholder={props.placeholder}
+        value = {inputValue}
+        onChange = { e => {setInputValue(e.target.value)} }
+        size = {props.size}
+        type="text"/>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Input } from '../components/Input';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (
     <>
       <h1>Hello World!</h1>
+      <Input id='iEmail' icon={faEnvelope} placeholder='Email'/>
     </>
   );
 }
