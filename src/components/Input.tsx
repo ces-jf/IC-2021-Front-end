4d51e4c4a00dca25d8fc2069806c4607ba6eb9b6
Natan Fernandes <natanfernandes@protonmail.com>
2021-08-21 23:29:15 -0300

    adicionado modal

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -19,26 +19,26 @@ export function Input(props: ButtonProps) {
   const span = useRef(null);
 
   return (
-    <>
-    <label htmlFor={props.id} className={styles.labelInput}>{props.labelText}</label>
-    <div onClick={ () => input.current.focus()} 
-      className={`${styles.inputContainer} ${inputValue.length > 0 ? styles.filled : null } ${shouldValidationMessageAppear ? styles.erro : null}`}>
-      <props.icon/>
-      <input id={props.id} ref={input}
-        value = {inputValue}
-        onChange = { e => {setInputValue(e.target.value); setShouldValidationMessageAppear(false)} }
-        size = {props.size}
-        type={props.type}
-        required={props.isRequired}
-        onBlur={ () => props.isRequired 
-          ? inputValue.length > 0
-            ? null
-            : setShouldValidationMessageAppear(true) 
-          : null}
-      />
-      <span ref={span}
-        className={`${styles.validationMessage} ${shouldValidationMessageAppear ? null : styles.displayNone}`}>Campo obrigatório</span>
-      </div>
-    </>
+    <div className={styles.container}>
+      <label htmlFor={props.id} className={styles.labelInput}>{props.labelText}</label>
+      <div onClick={ () => input.current.focus()} 
+        className={`${styles.inputContainer} ${inputValue.length > 0 ? styles.filled : null } ${shouldValidationMessageAppear ? styles.erro : null}`}>
+        <props.icon/>
+        <input id={props.id} ref={input}
+          value = {inputValue}
+          onChange = { e => {setInputValue(e.target.value); setShouldValidationMessageAppear(false)} }
+          size = {props.size}
+          type={props.type}
+          required={props.isRequired}
+          onBlur={ () => props.isRequired 
+            ? inputValue.length > 0
+              ? null
+              : setShouldValidationMessageAppear(true) 
+            : null}
+        />
+        <span ref={span}
+          className={`${styles.validationMessage} ${shouldValidationMessageAppear ? null : styles.displayNone}`}>Campo obrigatório</span>
+        </div>
+    </div>
   );
 }
